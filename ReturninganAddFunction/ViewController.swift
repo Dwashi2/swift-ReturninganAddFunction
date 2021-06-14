a167a1db5df02e6c1ccc29a287489b68adb1b9c3
Daniel Washington <danielvertigo@hotmail.com>
2021-06-14 10:15:03 -0300

    Files

--- ReturninganAddFunction/ViewController.swift
+++ ReturninganAddFunction/ViewController.swift
@@ -4,6 +4,22 @@
 //
 //  Created by Daniel Washington Ignacio on 14/06/21.
 //
+/*
+ Given a number, n, return a function which adds n to the number passed to it.
+
+ Examples
+
+ add(10)(20) ➞ 30
+
+ add(0)(20) ➞ 20
+
+ add(-30)(80) ➞ 50
+ Notes
+
+ All numbers used in the tests will be integers (whole numbers).
+ Returning a function from a function is a key part of understanding higher order functions (functions which operate on and return functions).
+ */
+
 
 import UIKit
 
@@ -12,8 +28,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(self.add(10)(20))
+        print(self.add(0)(20))
+        print(self.add(-30)(80))
     }
 
+    func add(_ n: Int) -> (Int) -> Int {
+        func sum(num: Int) -> Int{
+            return num + n
+        }
+        return sum
+    }
 
 }
 
